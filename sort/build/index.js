e137d96650bc9d6a7b738b77809dd2994983879e
Kobe Ruado <insidiae423@gmail.com>
2022-02-07 22:52:25 +0800

    Add initial sorting logic

--- sort/build/index.js
+++ sort/build/index.js
@@ -4,7 +4,25 @@ class Sorter {
         this.collection = collection;
     }
     sort() {
-        //TODO
+        const { length } = this.collection;
+        //* Bubble Sort
+        for (let i = 0; i < length; i++) {
+            for (let j = 0; j < length - i - 1; j++) {
+                //? This only works if collection is number[]
+                if (this.collection instanceof Array) {
+                    if (this.collection[j] > this.collection[j + 1]) {
+                        [this.collection[j], this.collection[j + 1]] = [
+                            this.collection[j + 1],
+                            this.collection[j],
+                        ];
+                    }
+                }
+                //? This only works if collection is string
+                if (typeof this.collection === "string") {
+                    // You get the idea... Yeah there has to be a better way than this!
+                }
+            }
+        }
     }
 }
 const sorter = new Sorter([10, 3, -5, 0]);
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,8 +1,28 @@
 class Sorter {
-  constructor(public collection: number[]) {}
+  constructor(public collection: number[] | string) {}
 
   sort(): void {
-    //TODO
+    const { length } = this.collection;
+
+    //* Bubble Sort
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
+        //? This only works if collection is number[]
+        if (this.collection instanceof Array) {
+          if (this.collection[j] > this.collection[j + 1]) {
+            [this.collection[j], this.collection[j + 1]] = [
+              this.collection[j + 1],
+              this.collection[j],
+            ];
+          }
+        }
+
+        //? This only works if collection is string
+        if (typeof this.collection === "string") {
+          // You get the idea... Yeah there has to be a better way than this!
+        }
+      }
+    }
   }
 }
 
