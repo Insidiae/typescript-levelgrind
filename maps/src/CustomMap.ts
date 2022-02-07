2eb0246c317d2ad4687f84e965b2e65aff925f77
Kobe Ruado <insidiae423@gmail.com>
2022-02-07 18:25:00 +0800

    Add marker popup

--- maps/src/CustomMap.ts
+++ maps/src/CustomMap.ts
@@ -1,11 +1,13 @@
 // import { User } from "./User";
 // import { Company } from "./Company";
 
-interface Mappable {
+export interface Mappable {
   location: {
     lat: number;
     lng: number;
   };
+
+  markerContent(): string;
 }
 
 export class CustomMap {
@@ -52,12 +54,20 @@ export class CustomMap {
 
   //* There we go. Much better!
   addMarker(mappable: Mappable): void {
-    new google.maps.Marker({
+    const marker = new google.maps.Marker({
       map: this._googleMap,
       position: {
         lat: mappable.location.lat,
         lng: mappable.location.lng,
       },
+    });
+
+    marker.addListener("click", () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: mappable.markerContent(),
+      });
+
+      infoWindow.open(this._googleMap, marker);
     });
   }
 }
