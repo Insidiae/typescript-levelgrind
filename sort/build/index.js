7bd9f6cb6cdbacbb582e3fa059851a11faf55d67
Kobe Ruado <insidiae423@gmail.com>
2022-02-07 22:29:19 +0800

    Initialize project

---
+++ sort/build/index.js
@@ -0,0 +1,12 @@
+"use strict";
+class Sorter {
+    constructor(collection) {
+        this.collection = collection;
+    }
+    sort() {
+        //TODO
+    }
+}
+const sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection);
